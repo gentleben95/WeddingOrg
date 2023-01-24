5066042dc786dbbd84d0b3184c40d92719417dfd
gentleben95 <111108615+gentleben95@users.noreply.github.com>
2023-01-24 09:02:34 +0100

    AddedMigration

--- WeddingOrg/Migrations/20230124080201_AzureInit.Designer.cs
+++ WeddingOrg/Migrations/20230124080201_AzureInit.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WeddingOrg.Data;
 
@@ -11,9 +12,11 @@ using WeddingOrg.Data;
 namespace WeddingOrg.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230124080201_AzureInit")]
+    partial class AzureInit
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
