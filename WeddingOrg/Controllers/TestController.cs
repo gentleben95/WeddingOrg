54f3dace7b490a22fe8ea86e5fa3322664dae176
gentleben95 <111108615+gentleben95@users.noreply.github.com>
2023-01-24 09:21:51 +0100

    New Controller - Test

---
+++ WeddingOrg/Controllers/TestController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WeddingOrg.DTOs;
+
+namespace WeddingOrg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            // your code to handle GET request
+            return Ok("Got it!");
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] string value)
+        {
+            // your code to handle POST request
+            return Ok("Received: " + value);
+        }
+    }
+}
