d3d05470ac41ebdfb73adfbc91f78c5f3e72fee7
gentleben95 <111108615+gentleben95@users.noreply.github.com>
2023-01-27 17:09:05 +0100

    reload page

---
+++ WeddingOrg/Migrations/20230127144142_Nullable.cs
@@ -0,0 +1,172 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WeddingOrg.Migrations
+{
+    /// <inheritdoc />
+    public partial class Nullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weddings_Brides_BrideId",
+                table: "Weddings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weddings_Grooms_GroomId",
+                table: "Weddings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroomId",
+                table: "Weddings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BrideId",
+                table: "Weddings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Instagram",
+                table: "Grooms",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Grooms",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Instagram",
+                table: "Brides",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Brides",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weddings_Brides_BrideId",
+                table: "Weddings",
+                column: "BrideId",
+                principalTable: "Brides",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weddings_Grooms_GroomId",
+                table: "Weddings",
+                column: "GroomId",
+                principalTable: "Grooms",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weddings_Brides_BrideId",
+                table: "Weddings");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Weddings_Grooms_GroomId",
+                table: "Weddings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "GroomId",
+                table: "Weddings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "BrideId",
+                table: "Weddings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Instagram",
+                table: "Grooms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Grooms",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Instagram",
+                table: "Brides",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Brides",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weddings_Brides_BrideId",
+                table: "Weddings",
+                column: "BrideId",
+                principalTable: "Brides",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Weddings_Grooms_GroomId",
+                table: "Weddings",
+                column: "GroomId",
+                principalTable: "Grooms",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- WeddingOrg/Models/Wedding.cs
+++ WeddingOrg/Models/Wedding.cs
@@ -6,9 +6,9 @@ namespace WeddingOrg.Models
     public class Wedding : Entity
     {
         public string DateOfTheWedding { get; set; }
-        public string DateOfSigningTheContract { get; set; }        
-        public Bride Bride { get; set; } = new();
-        public Groom Groom { get; set; } = new();   
+        public string DateOfSigningTheContract { get; set; }
+        public Bride? Bride { get; set; } = new();
+        public Groom? Groom { get; set; } = new();   
         public Photographer? Photographer { get; set; } = new();
         public Cameraman? Cameraman { get; set; } = new();
         public Restaurant? Restaurant { get; set; } = new();
