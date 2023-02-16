6db1ace7c3349987c3f9c45d119779a7567e1e75
gentleben95 <111108615+gentleben95@users.noreply.github.com>
2023-02-16 16:45:32 +0100

    Next

--- WeddingOrg.Application/Cameramen/Queries/GetCameramanByIdQuery.cs
+++ WeddingOrg.Application/Cameramen/Queries/GetCameramanByIdQuery.cs
@@ -13,34 +13,39 @@ using WeddingOrg.Domain.Entities;
 
 namespace WeddingOrg.Application.Cameramen.Queries
 {
-    public class GetCameramanByIdQuery : IRequest<CameramanDto>
-    {
-        public int Id { get; set; }
+    public record GetCameramanByIdQuery(int Id) : IRequest<CameramanDto>;
+    //public class GetCameramanByIdQuery : IRequest<CameramanDto>
+    //{
+    //    public int Id { get; set; }
 
-        public GetCameramanByIdQuery(int id)
-        {
-            Id = id;
-        }
-    }
+    //    public GetCameramanByIdQuery(int id)
+    //    {
+    //        Id = id;
+    //    }
+    //}
     public class GetCameramanByIdQueryHandler : IRequestHandler<GetCameramanByIdQuery, CameramanDto>
     {
         private readonly IWeddingsRepository _photographerRepository;
+        private readonly IMediator _mediator;
 
-        public GetCameramanByIdQueryHandler(IWeddingsRepository photographerRepository)
+        public GetCameramanByIdQueryHandler(IWeddingsRepository photographerRepository, IMediator mediator)
         {
             _photographerRepository = photographerRepository;
+            _mediator = mediator;
         }
 
         public async Task<CameramanDto> Handle(GetCameramanByIdQuery request, CancellationToken cancellationToken)
         {
-            var cameraman = await _photographerRepository.GetCameramanById(request.Id, cancellationToken);
-            if (cameraman == default)
-            {
-                throw new WeddingNotFoundException($"Photographer with {request.Id} does not exist.");
-            }
+            var result = await _mediator.Send(new GetCameramenQuery());
+            var output = result.
+            //var cameraman = await _photographerRepository.GetCameramanById(request.Id, cancellationToken);
+            //if (cameraman == default)
+            //{
+            //    throw new WeddingNotFoundException($"Photographer with {request.Id} does not exist.");
+            //}
 
-            var cameramanDto = new CameramanDto(cameraman.Name, cameraman.Facebook, cameraman.Instagram);
-            return cameramanDto;
+            //var cameramanDto = new CameramanDto(cameraman.Name, cameraman.Facebook, cameraman.Instagram);
+            //return cameramanDto;
         }
     }
 }
